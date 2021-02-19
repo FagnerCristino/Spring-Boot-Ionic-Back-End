71538402e5f5e809401187729a03e7cac77cf15e
Fagner Cristino <fcristino.sc48@gmail.com>
2021-02-19 11:38:33 -0300

    Testando a primeira classe de dominio - Categoria

---
+++ src/main/java/com/fagnerdev/cursomc/controllers/CategoriaController.java
@@ -0,0 +1,28 @@
+package com.fagnerdev.cursomc.controllers;
+
+import com.fagnerdev.cursomc.domain.Categoria;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaController {
+
+    @GetMapping
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+
+        return lista;
+    }
+}
