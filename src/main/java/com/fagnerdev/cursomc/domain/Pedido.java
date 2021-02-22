c049bb7757dbb5a87e78318481c4b949801e3e11
Fagner Cristino <fcristino.sc48@gmail.com>
2021-02-22 11:19:09 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/fagnerdev/cursomc/domain/Pedido.java
+++ src/main/java/com/fagnerdev/cursomc/domain/Pedido.java
@@ -3,7 +3,9 @@ package com.fagnerdev.cursomc.domain;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Pedido implements Serializable {
@@ -24,6 +26,9 @@ public class Pedido implements Serializable {
     @ManyToOne
     @JoinColumn(name = "endereco_de_entrega_id")
     private Endereco enderecoDeEntrega;
+
+    @OneToMany(mappedBy = "id.pedido")
+    private Set<ItemPedido> itens = new HashSet<>();
 
     public Pedido(){
     }
@@ -75,16 +80,14 @@ public class Pedido implements Serializable {
         this.enderecoDeEntrega = enderecoDeEntrega;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Pedido)) return false;
-        Pedido pedido = (Pedido) o;
-        return Objects.equals(getId(), pedido.getId()) && Objects.equals(getInstante(), pedido.getInstante()) && Objects.equals(getPagamento(), pedido.getPagamento()) && Objects.equals(getCliente(), pedido.getCliente()) && Objects.equals(getEnderecoDeEntrega(), pedido.getEnderecoDeEntrega());
+
+    public Set<ItemPedido> getItens() {
+        return itens;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getInstante(), getPagamento(), getCliente(), getEnderecoDeEntrega());
+    public void setItens(Set<ItemPedido> itens) {
+        this.itens = itens;
     }
+
+
 }
